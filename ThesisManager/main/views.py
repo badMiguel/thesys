1bc0020b9a40c0569e6165ede97293136fa13c5e
MiguelCDU <s365958@students.cdu.edu.au>
2024-03-29 00:43:45 +0930

    added classes

--- ThesisManager/main/views.py
+++ ThesisManager/main/views.py
@@ -13,5 +13,20 @@ class Thesis:
     def __init__(self, topic_number, casuarina, sydney, external, 
                  chem_engineering, civ_engineering, elec_engineering, mech_engineering, comp_sci, cyber_sec, data_sci, info_syst, software_engineer,
                  category,  title, thesis_supervisor, description):
-        pass
-        
+        self.topic_number = topic_number
+        self.casuarina = casuarina
+        self.sydney = sydney
+        self.external = external
+        self.chem_engineering = chem_engineering
+        self.civ_engineering = civ_engineering
+        self.elec_engineering = elec_engineering
+        self.mech_engineering = mech_engineering
+        self.comp_sci = comp_sci
+        self.cyber_sec = cyber_sec
+        self.data_sci = data_sci
+        self.info_syst = info_syst
+        self.software_engineer = software_engineer
+        self.category = category
+        self.title = title
+        self.thesis_supervisor = thesis_supervisor
+        self.description = description
