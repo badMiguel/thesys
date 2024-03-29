9b4f42fad5c59975660e7be52562712ad2490eab
MiguelCDU <s365958@students.cdu.edu.au>
2024-03-30 01:33:23 +0930

    class

--- ThesisManager/main/views.py
+++ ThesisManager/main/views.py
@@ -10,24 +10,88 @@ def home(request):
 
 
 class Thesis:
-    def __init__(self, topic_number, casuarina, sydney, external, 
-                 chem_engineering, civ_engineering, elec_engineering, mech_engineering, comp_sci, cyber_sec, data_sci, info_syst, software_engineer,
-                 category,  title, thesis_supervisor, description):
-        
+    def __init__(self, topic_number, title, campus, course, category, supervisor):
+            
         self.topic_number = topic_number
-        self.casuarina = casuarina
-        self.sydney = sydney
-        self.external = external
-        self.chem_engineering = chem_engineering
-        self.civ_engineering = civ_engineering
-        self.elec_engineering = elec_engineering
-        self.mech_engineering = mech_engineering
-        self.comp_sci = comp_sci
-        self.cyber_sec = cyber_sec
-        self.data_sci = data_sci
-        self.info_syst = info_syst
-        self.software_engineer = software_engineer
-        self.category = category
         self.title = title
-        self.thesis_supervisor = thesis_supervisor
-        self.description = description
+        self.campus = campus
+        self.course = course
+        self.category = category
+        self.supervisor = supervisor
+
+def thesis_list():
+    topics = []
+    
+    campuses = ['Casuarina', 'Sydney', 'External']
+    area = {
+        'chemical': 'Chemical Engineering',
+        'civil' : 'Civil and Structural Engineering',
+        'electrical' :'Electrical and Electronics Engineering',
+        'mechanical' : 'Mechanical Engineering',
+        'computer' : 'Computer Science',
+
+        'data' : 'Data Science',
+        'information' : 'Information Systems and Data Science',
+        'software' : 'Software Engineering',
+        } 
+    categories = {
+        'artificial' : 'Artificial Intelligence, Machine Learning and Data Science',
+        'biomedical' : 'Biomedical Engineering and Health Informatics',
+        'cyber' : 'Cyber Security',
+    }
+
+    topics.append(Thesis(
+        topic_number=1,       
+        title= 'Machine learning approaches for Cyber Security',
+        campus = campuses,
+        course= [area['computer'], area['software']],
+        category= categories['artificial'],
+        supervisor= 'Bharanidharan Shanmugam'
+    )) 
+     
+    topics.append(Thesis(
+        topic_number= 9,
+        title= 'Informetrics applications in multidisciplinary domain',
+        campus= campuses,
+        course= [area['computer'], area['cyber'], area['data'], area['information'], area['software']],
+        category= categories['artificial'],
+        supervisor= 'Yakub Sebastian',
+    ))
+     
+    topics.append(Thesis(
+        topic_number=16,
+        title='Development of a Virtual Reality System to Test Binaural Hearing',
+        campus=[campuses[0], campuses[2]],
+        course=[area['electrical'], area['computer'],  area['software']],
+        category=categories['biomedical'],
+        supervisor='Sami Azam',
+    ))
+    
+    topics.append(Thesis(
+        topic_number=41,
+        title='Current trends on cryptomining and its potential impact on cryptocurrencies',
+        campus=campuses,
+        course=[area['computer'], area['cyber'], area['software']],
+        category= categories['cyber'],
+        supervisor= 'Sami Azam',
+    ))
+    
+    topics.append(Thesis(
+        topic_number=,
+        title=,
+        campus=,
+        course=,
+        category=,
+        supervisor=,
+    ))
+    
+    topics.append(Thesis(
+        topic_number=,
+        title=,
+        campus=,
+        course=,
+        category=,
+        supervisor=,
+    ))    
+    
+    
