d97d3eb999a46a48d508698b5bc9b5c50a663efc
MiguelCDU <s365958@students.cdu.edu.au>
2024-03-30 16:11:55 +0930

    updated class

--- ThesisManager/main/views.py
+++ ThesisManager/main/views.py
@@ -17,7 +17,7 @@ def thesis_list(request):
     
 
 class Thesis:
-    def __init__(self, topic_number, title, campus, course, category, supervisor):
+    def __init__(self, topic_number, title, campus, course, category, supervisor, description):
             
         self.topic_number = topic_number
         self.title = title
@@ -25,6 +25,7 @@ class Thesis:
         self.course = course
         self.category = category
         self.supervisor = supervisor
+        self.description = description 
         
 def create_thesis():
     topics = []
@@ -53,7 +54,8 @@ def create_thesis():
         campus = campuses,
         course= [area['computer'], area['software']],
         category= categories['artificial'],
-        supervisor= 'Bharanidharan Shanmugam'
+        supervisor= 'Bharanidharan Shanmugam',
+        description='As we use internet more, the data produced by us is enormous. But are these data being secure? The goal of applying machine learning or intelligence is to better risk modelling and prediction and for an informed decision support. Students will be working with either supervised or unsupervised machine learning approaches to solve the problem/s in the broader areas of Cyber Security.'
     )) 
      
     topics.append(Thesis(
@@ -63,6 +65,7 @@ def create_thesis():
         course= [area['computer'], area['cyber'], area['data'], area['information'], area['software']],
         category= categories['artificial'],
         supervisor= 'Yakub Sebastian',
+        description='Informetrics studies are concerned with the quantitative aspects of information. The applications of advanced machine learning, information retrieval, network science and bibliometric techniques on various information artefact have contributed fresh insights into the evolutionary nature of research fields. This project aims at discovering informetric properties of multidisciplinary research literature using various machine learning, network analysis, data visualisation and data wrangling tools.'
     ))
      
     topics.append(Thesis(
@@ -72,6 +75,7 @@ def create_thesis():
         course=[area['electrical'], area['computer'],  area['software']],
         category=categories['biomedical'],
         supervisor='Sami Azam',
+        description='A virtual reality system could be used to objectively test the binaural hearing ability of humans (the ability to hear stereo and locate the direction and distance of sound). This project aims to design, implement and evaluate a VR system using standard off the shelf components (VR goggle and headphones) and standard programming techniques.'
     ))
     
     topics.append(Thesis(
@@ -81,6 +85,7 @@ def create_thesis():
         course=[area['computer'], area['cyber'], area['software']],
         category= categories['cyber'],
         supervisor= 'Sami Azam',
+        description= "Cryptomining is the process of mining crypto currencies by running a sequence of algorithms. Traditionally, to mine new crypto coins, a person (or group of people) would buy expensive computers and spend a lot of time and money running them to perform the difficult calculations to generate crypto coins. Some website owners have started taking a different approach; they have put the software which runs those difficult calculations into their website's Javascript. This then causes the computers belonging to the visitors of their website to run those calculations for them, instead of running them themselves. In other words, when you visit a website with an embedded crypto-miner in it, your computer and electricity is used to try to generate crypto-coins for the owners of that website. Although there are various measures being applied to stop these illegitimate minings, the trend is still increasing. This research aims to find out potential gaps in current methodologies and develop a solution that can fulfil the gap. It also aims to find out: (1) What type crypto mining methodologies are being applied?, (2) Apart from crypto-mining, what other security risks may it introduce? For example: cryptojacking,  and (3) How current web standards are tackling this problem?"
     ))
     
     topics.append(Thesis(
@@ -90,6 +95,7 @@ def create_thesis():
         course= [area['electrical'], area['computer'], area['data'], area['software']],
         category= categories['artificial'],
         supervisor= 'Asif Karim',
+        description='The project aims to use multiple publicly available health datasets to formulate a different dataset that may have features from the original set along with new ones developed through feature engineering. The dataset will then be used to build predictive model based on both general and deep learning based algorithm. The findings will be analysed and compared to similar research works.'
     ))
     
     topics.append(Thesis(
@@ -99,6 +105,7 @@ def create_thesis():
         course= [area['electrical'], area['computer'], area['data'], area['software']],
         category= categories['artificial'],
         supervisor='Asif Karim',
+        description='The proposed system will present a two-cluster solution from a given dataset which will group toddlers based on multiple common medical traits. In depth literature survey of similar studies, both supervised and unsupervised will be carried out before the cluster-based model is implemented. The solution will be validated using both External and Internal validation measures and statistical significance tests.'
     ))    
 
     topics.append(Thesis(
@@ -108,6 +115,7 @@ def create_thesis():
         course= [area['chemical'], area['civil'], area['computer'], area['cyber'], area['data'], area['electrical'], area['information'], area['mechanical'], area['software']],
         category= categories['artificial'],
         supervisor= 'Bharanidharan Shanmugam',
+        description='Artificial Intelligence has been used in many applications to solve certain problems through out the academia and the industry – from electricity to writing text. AI has a multitude of applications and this project will pick up a problem and explore the applications of AI with minimal human intervention. Examples of applications include -building a bot, predicting the power usage, spam filtering and the list is endless.'
     ))    
     
     return topics
