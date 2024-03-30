cc13d44291006fcc83517c25139646e63b27afbf
MiguelCDU <s365958@students.cdu.edu.au>
2024-03-30 21:37:16 +0930

    limited the word displayed on description to 50 words

--- ThesisManager/main/views.py
+++ ThesisManager/main/views.py
@@ -10,8 +10,25 @@ def thesis_details(request):
     pass
 
 def thesis_list(request):
-        
-    return render(request, 'main/thesis_list.html', {'theses': create_thesis()})
+    theses = create_thesis()
+    
+    for thesis in theses:
+        description = thesis.description
+        word_count = description.split()
+        if len(word_count) > 50:
+            description = ' '.join(word_count[:50])
+            
+            punctuation = ['.', ',', '/', ';', ':']
+            if description[-1] in punctuation:
+                description = description[:-1] + '...'
+            else:
+                description = description + '...'
+                
+            thesis.description = description
+                
+    context = {'theses': theses}
+    
+    return render(request, 'main/thesis_list.html', context)
     
        
     
