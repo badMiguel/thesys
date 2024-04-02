0e409e698bb548dd0a03908fc3e5dea0499cf657
MiguelCDU <s365958@students.cdu.edu.au>
2024-04-02 17:10:11 +0930

    Deleted models. moved it to different py file instead

--- ThesisManager/main/views.py
+++ ThesisManager/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Thesis, create_thesis, delete_data
+from .data import create_thesis
 
 def home(request):
     return render(request, 'main/home.html')
@@ -11,44 +11,24 @@ def thesis_details(request):
     pass
 
 def thesis_list(request):
-    # Deletes any existing data to avoid duplication. 
-    delete_data()
 
-    # Creates thesis list
-    create_thesis()
+    theses = create_thesis()
     
-    theses = Thesis.objects.all() 
-
-    # Structure the data into a dictionary nested inside a list
-    thesis_data = []
     for thesis in theses:
-        thesis_dict = {
-        'topic_number': thesis.topic_number,
-        'title': thesis.title,
-        'category': thesis.category,
-        'supervisor': thesis.supervisor,
-        'description': thesis.description,
-        'campuses': [campus.campus for campus in thesis.campus.all()],
-        'courses': [course.course for course in thesis.course.all()]
-        }
-        thesis_data.append(thesis_dict)
-    
-    # Adds a maximum word count displayed for the description (50 words)
-    for thesis in thesis_data:
-        description = thesis['description']
+        description = thesis.description
         word_count = description.split()
         if len(word_count) > 50:
             description = ' '.join(word_count[:50])
-            punctuation = ['.', ',', '/', ';', ':']
             
+            punctuation = ['.', ',', '/', ';', ':']
             if description[-1] in punctuation:
                 description = description[:-1] + '...'
             else:
                 description = description + '...'
                 
-        thesis['description'] = description
-
-    context = {'theses': thesis_data}  
+            thesis.description = description
+                
+    context = {'theses': theses}
     
     return render(request, 'main/thesis_list.html', context)
     
