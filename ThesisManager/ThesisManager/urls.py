beef0695a87eb0505b5ebcc294a0bee2dc1f9688
MiguelCDU <s365958@students.cdu.edu.au>
2024-04-12 16:59:22 +0930

    .

--- ThesisManager/ThesisManager/urls.py
+++ ThesisManager/ThesisManager/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/home/')),
     path('home/', views.home, name='home'),
     path('thesis/', views.thesis_list, name= 'thesis_list'),
+    path('thesis/<int:topic_number>/', views.thesis_details, name= 'thesis_details'),
     path('aboutus/', views.about_us, name= 'about_us')
     
     # re_path(r'^/?$', views.) if re_path is needed
