0bc0d0aab4d3735725bb53448400a893812ab078
MiguelCDU <s365958@students.cdu.edu.au>
2024-05-12 20:59:33 +0930

    updated models

--- ThesisManager/main/admin.py
+++ ThesisManager/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Campus, Course, Category, Supervisor, Thesis
+from .models import Campus, Course, Category, Supervisor, Thesis, ThesisRequest, GroupApplication
 
 # Register your models here.
 
@@ -8,3 +8,5 @@ admin.site.register(Course)
 admin.site.register(Category)
 admin.site.register(Supervisor)
 admin.site.register(Thesis)
+admin.site.register(ThesisRequest)
+admin.site.register(GroupApplication)
--- ThesisManager/main/models.py
+++ ThesisManager/main/models.py
@@ -90,42 +90,40 @@ class Thesis(models.Model):
     supervisor = models.ForeignKey(Supervisor, on_delete= models.PROTECT, verbose_name='Supervisor Name')
     course = models.ManyToManyField(Course, verbose_name='Course Name')
     campus = models.ManyToManyField(Campus, verbose_name='Campus Name',)
-    #date_created = models.DateTimeField(auto_now_add=True, verbose_name = 'Date Created')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name = 'Date Created')
     last_edited = models.DateTimeField(auto_now=True, verbose_name = 'Date Edited')
-    # group_taker_limit = models.IntegerField(
-    #     validators=[
-    #         MinValueValidator(0),
-    #         MaxValueValidator(20),
-    #     ], 
-    #     verbose_name='Number Limit of group takers', 
-    #     null = True
-    # )
+    group_taker_limit = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(20),
+        ], 
+        verbose_name='Number Limit of group takers', 
+        null = True
+    )
     
     def __str__(self):
         return str(self.topic_number) + ' - ' + self.title 
         
-
-# class  ThesisRequest(models.Model):
-#     thesis_request_number = models.IntegerField( 
-#         verbose_name= 'Thesis Request',
-#         primary_key=True,
-#     )
-#     requested_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Requested by')
-#     request_date = models.DateTimeField(auto_now_add=True, verbose_name='Request Date')
-#     thesis_request = models.ForeignKey(Thesis, on_delete = models.CASCADE, verbose_name='Thesis Request')
+class  ThesisRequest(models.Model):
+    thesis_request_number = models.AutoField( 
+        verbose_name= 'Thesis Request',
+        primary_key=True,
+    )
+    requested_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Requested by', default='')
+    request_date = models.DateTimeField(auto_now_add=True, verbose_name='Request Date')
+    thesis_request = models.ForeignKey(Thesis, on_delete = models.CASCADE, verbose_name='Thesis Request', default='')
     
-#     def __str__(self):
-#         return self.requested_by + ' - ' + self.thesis_request
+    def __str__(self):
+        return f'{self.requested_by} - {self.thesis_request}'
        
-# class GroupApplication(models.Model):
-#     group_application_number = models.AutoField(
-#         verbose_name='Group Application Number', 
-#         primary_key=True
-#     )
-        
-#     group = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Group', default='')
-#     thesis = models.ForeignKey(Thesis, on_delete=models.PROTECT, verbose_name='Thesis', default='')
-#     application_date = models.DateTimeField(auto_now_add=True)
+class GroupApplication(models.Model):
+    group_application_number = models.AutoField(
+        verbose_name='Group Application Number', 
+        primary_key=True
+    )
+    group = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Group', default='')
+    thesis = models.ForeignKey(Thesis, on_delete=models.PROTECT, verbose_name='Thesis', default='')
+    application_date = models.DateTimeField(auto_now_add=True)
     
-#     def __str__(self):
-#         return self.group + ' - ' + self.thesis
+    def __str__(self):
+        return f'{self.group} - {self.thesis}'
